e4efc05cca0c860b00dd8c5d09847775e4a33273
Abusaleh Md Nayeem <nayeemmbstu@gmail.com>
2022-02-24 08:31:24 +0600

    simple change

--- EventBus/GlobalUsings.cs
+++ EventBus/GlobalUsings.cs
@@ -1,5 +1,2 @@
-﻿global using System.Collections.Generic;
+﻿global using System;
 global using System.Linq;
-global using System.Text.Json.Serialization;
-global using System.Threading.Tasks;
-global using System;
---
+++ WebHost.Customization/Services/WebHostSwaggerServiceConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebHost.Customization.Services
+{
+    public static class WebHostSwaggerServiceConfiguration
+    {
+        private static string GetDocumentName(this IConfiguration configuration) => configuration["Swagger:Document:Name"];
+
+        private static string GetDocumentTitle(this IConfiguration configuration) => configuration["Swagger:Document:Title"];
+
+        private static string GetDocumentVersion(this IConfiguration configuration) => configuration["Swagger:Document:Version"];
+
+        public static IServiceCollection SwaggerServiceConfiguration(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(configuration.GetDocumentName(),
+                    new OpenApiInfo
+                    { Title = configuration.GetDocumentTitle(), Version = configuration.GetDocumentVersion() });
+            });
+            return services;
+        }
+    }
+}
