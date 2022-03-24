228b8f6e84d870e4aad93a5db77e22a326f4c46e
Abusaleh Md Nayeem <nayeemmbstu@gmail.com>
2022-03-24 10:54:28 +0600

    minor change

---
+++ wsm-client/src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html'
+})
+export class AppNavMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
