ce8089459c4149ed435e752b216afa00ce900aeb
Abusaleh Md Nayeem <nayeemmbstu@gmail.com>
2023-05-26 21:52:42 +0600

    update project

--- wsm-client/src/app/app-routing.module.ts
+++ wsm-client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'false', loadChildren: () => import('./shared-kernels/shared-kernels.module').then(m => m.SharedKernelsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
